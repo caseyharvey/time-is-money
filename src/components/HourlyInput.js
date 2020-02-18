e75c262737b9827d6ee3107357d152a3769eda2b
hero0nezero <caseyharvey1@gmail.com>
2020-02-18 16:58:07 +1100

    HourlyInput wired and working. Error message css update

--- src/components/HourlyInput.js
+++ src/components/HourlyInput.js
@@ -1,29 +1,59 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
-import { setHourly } from "../actions";
 import { connect } from "react-redux";
+import { setHourly } from "../actions";
+import { Field, reduxForm, reset } from "redux-form";
 
 class HourlyInput extends React.Component {
-  renderInput = ({ input }) => {
+  renderInput = ({ input, meta: { error, invalid, pristine } }) => {
+    const errorClass = `${error && invalid && !pristine ? "errorMessage" : ""}`;
     return (
-      <input {...input} autoComplete="off" placeholder="Enter hourly rate" />
+      <div>
+        <span className="dollarSign">
+          <input
+            {...input}
+            autoComplete="off"
+            placeholder=" Enter hourly rate"
+            onFocus={e => e.target.select()}
+          />
+        </span>
+        <span className={errorClass}>{errorClass ? error : null}</span>
+      </div>
     );
   };
 
   onSubmit = value => {
-    console.log(value.inputHourlyRate);
-    this.props.setHourly(parseInt(value.inputHourlyRate));
+    if (this.props.valid) {
+      this.props.setHourly(parseInt(value.hourlyRate));
+    }
   };
 
   render() {
     return (
       <form onSubmit={this.props.handleSubmit(this.onSubmit)}>
-        <Field name="inputHourlyRate" component={this.renderInput} />
+        <Field name="hourlyRate" component={this.renderInput} />
         <button onClick={this.onSubmit}>Set</button>
       </form>
     );
   }
 }
+
+const validate = ({ hourlyRate }) => {
+  const errors = {};
+  if (!hourlyRate) {
+    errors.hourlyRate = "Enter hourly rate";
+  } else if (isNaN(Number(hourlyRate))) {
+    errors.hourlyRate = "Must be a number";
+  } else if (hourlyRate.length > 9) {
+    errors.hourlyRate = "WOW!";
+  } else if (/\s/.test(hourlyRate)) {
+    errors.hourlyRate = "No spaces allowed";
+  }
+  return errors;
+};
+
+const successfulSubmit = (result, dispatch) => {
+  dispatch(reset("HourlyInput"));
+};
 
 const mapStateToProps = state => {
   return { hourlyRate: state.hourlyRate };
@@ -34,51 +64,7 @@ const connectedHourlyInput = connect(mapStateToProps, { setHourly })(
 );
 
 export default reduxForm({
-  form: "HourlyInput"
+  form: "HourlyInput",
+  onSubmitSuccess: successfulSubmit,
+  validate
 })(connectedHourlyInput);
-
-// class Inputs extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { inputField: "" };
-//     this.textInput = React.createRef();
-//   }
-
-//   handleChange = event => {
-//     this.setState({ inputField: parseInt(event.target.value) });
-//   };
-
-//   setHourlyRate = e => {
-//     e.preventDefault();
-//     this.props.setHourly(this.state.inputField);
-//     this.textInput.current.value = "";
-//   };
-
-//   render() {
-//     return (
-//       <div>
-//         <div className="textField">
-//           <input
-//             ref={this.textInput}
-//             onChange={this.handleChange}
-//             value={this.state.textField}
-//             type="text"
-//             placeholder="Enter hourly rate"
-//           />
-//           <button onClick={this.setHourlyRate}>Set</button>
-//         </div>
-//         <div className="startStopBtn">
-//           <button>Start Total</button>
-//           <button>Start Task</button>
-//           <h4>${this.props.hourlyRate} an hour</h4>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// const mapStateToProps = state => {
-//   return { hourlyRate: state.hourlyRate };
-// };
-
-// export default connect(mapStateToProps, { setHourly })(Inputs);
--- src/components/ValueBreakdown.js
+++ src/components/ValueBreakdown.js
@@ -3,21 +3,15 @@ import { connect } from "react-redux";
 
 class ValueBreakdown extends React.Component {
   render() {
+    const hourlyRate = this.props.hourlyRate;
     return (
       <div className="valueBreakdown">
-        <div className="valueItem">${this.props.hourlyRate * 1} /hr</div>
+        <div className="valueItem">${hourlyRate * 1} /hr</div>
         <div className="valueItem">
-          $
-          {Math.round((this.props.hourlyRate / 60 + Number.EPSILON) * 100) /
-            100}{" "}
-          /min
+          ${Math.round((hourlyRate / 60 + Number.EPSILON) * 100) / 100} /min
         </div>
         <div className="valueItem">
-          $
-          {Math.round(
-            (this.props.hourlyRate / 60 / 60 + Number.EPSILON) * 100
-          ) / 100}{" "}
-          /sec
+          ${Math.round((hourlyRate / 3600 + Number.EPSILON) * 100) / 100} /sec
         </div>
       </div>
     );
